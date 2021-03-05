d0befccc2ced88dce3dd4e52fea75fe466e035d8
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-03-05 20:59:06 +0200

    to do list part 2

--- To-Do-List/app.js
+++ To-Do-List/app.js
@@ -2,9 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+let items = ["buy butter", "buy milk", "do laundry"];
 
-app.get("/", function(req, res) {
-    res.send("hello");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+
+let today = new Date();
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+
+let day = today.toLocaleDateString("en-Us", options);
+
+res.render("list", {kindOfDay: day, newListItems: items});
+
+})
+
+app.post("/", function(req, res) {
+    let item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
 })
 
 app.listen(3000, function(){
