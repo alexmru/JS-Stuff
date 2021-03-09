544f4a90eae20a4571d7d53ee5ebf77504a2555c
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-03-09 17:59:35 +0200

    exercitiu popup

---
+++ popUp/popup.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-wrapper');
+const exit = document.querySelector('.popup-close');
+
+
+button.addEventListener('click', (e) => {
+    popup.style.display = 'block';
+})
+
+exit.addEventListener('click',() => {
+    popup.style.display = "none";
+} )
+
+popup.addEventListener('click',() => {
+    popup.style.display = "none";
+} )
