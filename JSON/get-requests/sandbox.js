675aa0274527928d3b73630eb55ace4d02828bd6
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-03-13 22:11:37 +0200

    Async JS and callback functions

---
+++ JSON/get-requests/sandbox.js
@@ -0,0 +1,42 @@
+
+// Old Method!!
+
+// const getTodos = (resource) => {
+//     const request = new XMLHttpRequest();
+
+//     return new Promise((resolve, reject) => {
+//         request.addEventListener('readystatechange', () => {
+//             // console.log(request, request.readyState);
+//             if (request.readyState === 4 && request.status === 200) {
+//                 const data = JSON.parse(request.responseText);
+//                 resolve(data)
+//             }else if (request.readyState === 4) {
+//                 reject('error getting resource')
+//             }
+//         })
+        
+//         request.open('GET', resource);
+//         request.send();
+//     })
+
+    
+// };
+
+// getTodos("https://jsonplaceholder.typicode.com/todos").then((data) => {
+//     console.log("Promise was resolved :", data);
+// }).catch(err => {
+//     console.log("Promise was rejected", err)
+// })
+
+
+// New Method !
+
+fetch('https://jsonplaceholder.typicode.com/todos').then((response) => {
+    console.log("resolved!", response);
+    console.log(response.status);
+    return response.json();
+}).then((data) => {
+    console.log(data);
+}).catch((err) => {
+    console.log("rejected!", err);
+});
