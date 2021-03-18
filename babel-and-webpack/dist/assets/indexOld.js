d5a2ea60009c805ff9b4c7cbda613186b7697c38
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-03-18 07:02:42 +0200

    babel introducere

---
+++ babel-and-webpack/dist/assets/indexOld.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var greet = function greet(nume) {
+  console.log("hello ".concat(nume));
+};
+
+greet('Mario');
+greet('Luigi');
+greet('Johnny');
+greet('Marian');
+greet('Cati');
---
+++ babel-and-webpack/src/index.js
@@ -0,0 +1,10 @@
+const greet = nume => {
+    console.log(`hello ${nume}`);
+}
+
+greet('Mario');
+greet('Luigi');
+greet('Johnny');
+greet('Marian');
+greet('Cati');
+
