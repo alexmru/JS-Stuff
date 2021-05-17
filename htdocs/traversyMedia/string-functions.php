ea99c68aa798dd0e151d596188c4d11431906309
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-05-17 09:37:29 +0300

    Create string-functions.php

---
+++ htdocs/traversyMedia/string-functions.php
@@ -0,0 +1,16 @@
+<?php 
+// FUNCTII PENTRU STRING-uri
+
+$string = 'Alexandru';
+
+$output = substr('Alexandru', 1, 3);
+
+echo $output;
+echo '<br>';
+
+$length = strlen($string);
+
+echo $length;
+echo '<br>';
+
+$output = strpos();
