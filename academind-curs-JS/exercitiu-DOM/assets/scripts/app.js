e20bcf2e12e7772a84b9864880f8df034c9a0143
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-07-05 09:23:05 +0300

    incept exercitiu dom

---
+++ academind-curs-JS/exercitiu-DOM/assets/scripts/app.js
@@ -0,0 +1,8 @@
+let modal = document.getElementById("add-modal");
+
+let startAddMovieButton = document.querySelector('header button');
+
+
+startAddMovieButton.addEventListener('click', () => {
+    modal.classList.add('visible');
+})
